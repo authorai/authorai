7bb4dcf93f1259bbde18c40ff583cbb78b599316
authorai <119822081+authorai@users.noreply.github.com>
2022-12-22 20:09:47 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,10 @@
 # Configuration file for the Sphinx documentation builder.
 
+# -- Path setup ----------------------------------------
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
 # -- Project information
 
 project = 'AuthorAI'
